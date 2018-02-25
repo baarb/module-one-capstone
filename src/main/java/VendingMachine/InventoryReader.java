4d6243d8aa4270430797a4b526a708f81b8b09ae
Barbara Irons <birons89@gmail.com>
2018-02-25 13:41:53 -0500

    condensed InventoryReader logic, changed HashMap to TreeMap

--- src/main/java/VendingMachine/InventoryReader.java
+++ src/main/java/VendingMachine/InventoryReader.java
@@ -3,10 +3,10 @@ package VendingMachine;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.TreeMap;
 
 import Items.Beverages;
 import Items.Candy;
@@ -14,63 +14,40 @@ import Items.Chips;
 import Items.Gum;
 import Items.Item;
 
-public class InventoryReader
-{
-	public Map<String, Stack<Item>> generateInventory() throws FileNotFoundException
-	{
+public class InventoryReader {
+	public Map<String, Stack<Item>> generateInventory() throws FileNotFoundException {
 		File inventoryCSV = new File("vendingmachine.csv");
-		Map<String, Stack<Item>> inventory = new HashMap<>();
-		try (Scanner fileScanner = new Scanner(inventoryCSV))
-		{
-			while (fileScanner.hasNextLine())
-			{
+		Map<String, Stack<Item>> inventory = new TreeMap<>();
+		
+		try (Scanner fileScanner = new Scanner(inventoryCSV)) {
+			
+			while (fileScanner.hasNextLine()) {
 				Stack<Item> itemStack = new Stack<>();
 				String line = fileScanner.nextLine();
 				String[] lineArray = line.trim().split("\\|");
 				String mapLocation = lineArray[0];
 				String itemName = lineArray[1];
 				BigDecimal price = new BigDecimal(lineArray[2]);
-				if (mapLocation.startsWith("A"))
-				{
-					Chips tempItem = new Chips(itemName, price);
-					for (int i = 0; i < 5; i++)
-					{
-						itemStack.push(tempItem);
-					}
-					inventory.put(mapLocation, itemStack);
+				Item tempItem;
+				
+				if (mapLocation.startsWith("A")) {
+					tempItem = new Chips(itemName, price);
+				} else if (mapLocation.startsWith("B")) {
+					tempItem = new Candy(itemName, price);
+				} else if (mapLocation.startsWith("C")) {
+					tempItem = new Beverages(itemName, price);
+				} else {
+					tempItem = new Gum(itemName, price);
 				}
-				else if (mapLocation.startsWith("B"))
-				{
-					Candy tempItem = new Candy(itemName, price);
-					for (int i = 0; i < 5; i++)
-					{
-						itemStack.push(tempItem);
-					}
-					inventory.put(mapLocation, itemStack);
+				
+				for (int i = 0; i < 5; i++) {
+					itemStack.push(tempItem);
 				}
-				else if (mapLocation.startsWith("C"))
-				{
-					Beverages tempItem = new Beverages(itemName, price);
-					for (int i = 0; i < 5; i++)
-					{
-						itemStack.push(tempItem);
-					}
-					inventory.put(mapLocation, itemStack);
-				}
-				else
-				{
-					Gum tempItem = new Gum(itemName, price);
-					for (int i = 0; i < 5; i++)
-					{
-						itemStack.push(tempItem);
-					}
-					inventory.put(mapLocation, itemStack);
-				}
+				
+				inventory.put(mapLocation, itemStack);
 			}
 			return inventory;
 		}
 	}
 }
 
-// Should be able to return a full map of inventory
-//
